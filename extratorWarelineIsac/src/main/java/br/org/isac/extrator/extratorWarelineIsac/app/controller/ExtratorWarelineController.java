90617bfe1f33791b59b9cb9c2323202ace702883
fabio.alves <fabio.alves@HMA-RDPSERV-01.HMA.LOCAL>
2021-09-28 20:06:43 -0300

    versao 001_2021_09_28_2002

--- extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/controller/ExtratorWarelineController.java
+++ extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/controller/ExtratorWarelineController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.org.isac.extrator.extratorWarelineIsac.app.mysql.entity.CadFuncMySql;
 import br.org.isac.extrator.extratorWarelineIsac.app.mysql.repository.CadFuncMySqlRepository;
 import br.org.isac.extrator.extratorWarelineIsac.app.postgre.entity.CadFuncPostGre;
 import br.org.isac.extrator.extratorWarelineIsac.app.postgre.repository.CadFuncPostGreRepository;
@@ -32,8 +33,33 @@ public class ExtratorWarelineController {
 	@GetMapping(value = "/start")
 	public ModelAndView start(ModelMap model, HttpSession session) {
 
-		//List<CadFuncPostGre> cads = cadFuncPostgreeRepo.findAll();
-		//model.addAttribute("cads", cads);
+		List<CadFuncPostGre> cads = cfPRepo.findAll();
+		model.addAttribute("cads", cads);
+		return new ModelAndView("index", model);
+	}
+	
+	@GetMapping(value = "/start2")
+	public ModelAndView start2(ModelMap model, HttpSession session) {
+
+		List<CadFuncMySql> cads = cfMsRepo.findAll();
+		model.addAttribute("cads2", cads);
+		return new ModelAndView("index", model);
+	}
+	
+	@GetMapping(value = "/saveMysql")
+	public ModelAndView saveMysql(ModelMap model, HttpSession session) {
+		
+		CadFuncMySql c = new CadFuncMySql();
+		c.setCodprest("1795");
+		c.setDatnasfunc("23/06/1984");
+		c.setEnderfunc("GFDGHGH");
+		c.setEstcivfunc("F");
+		c.setSexofunc("F");
+				
+		cfMsRepo.save(c);
+
+		List<CadFuncMySql> cads = cfMsRepo.findAll();
+		model.addAttribute("cads2", cads);
 		return new ModelAndView("index", model);
 	}
 	
--- extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/mysql/entity/CadFuncMySql.java
+++ extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/mysql/entity/CadFuncMySql.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "cadfunc")
+@Table(name = "zz_cadfunc")
 public class CadFuncMySql {
 	
 	@Id
