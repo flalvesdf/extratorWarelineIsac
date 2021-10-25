127e90ec963ada98780689815667709272163fff
fabioalves <fabioalves@localhost>
2021-10-24 21:52:18 -0300

    versao 002_2021.10.24_2152

--- extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/mysql/repository/CadJuridMySqlRepository.java
+++ extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/mysql/repository/CadJuridMySqlRepository.java
@@ -3,9 +3,11 @@ package br.org.isac.extrator.extratorWarelineIsac.app.mysql.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.org.isac.extrator.extratorWarelineIsac.app.mysql.entity.CadJuridWareline;
 
+@Transactional
 public interface CadJuridMySqlRepository extends JpaRepository<CadJuridWareline, Integer> {
 	@Modifying
 	@Query("delete from CadJuridWareline u where u.unidade = ?1")
