9d0f4771e4315ddceeb0a96d2378e3f47a89b9a0
fabioalves <fabioalves@192.168.2.199>
2021-11-23 19:43:08 -0300

    versao 2021.11.23_1943_teste_anatolio

--- extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/conversores/Parametros.java
+++ extratorWarelineIsac/src/main/java/br/org/isac/extrator/extratorWarelineIsac/app/conversores/Parametros.java
@@ -10,7 +10,7 @@ public class Parametros {
 	 * UNDIDADE_SCHEDULE > para localizar a hora de executar o agendamento. mesmo para servidores com mais de uma unidade, deve ser informada apenas UMA;
 	 * IDS_UNIDADES > Para localizar as undidades que o servidor deve executar. Deve-se incluir todas, no caso de servidor que contenha mais de uma unidade;
 	 */
-	public static final Integer UNDIDADE_SCHEDULE = 12;
-	private static final Integer[] IDS_UNIDADES = {12};
+	public static final Integer UNDIDADE_SCHEDULE = 16;
+	private static final Integer[] IDS_UNIDADES = {16};
 	public static final List<Integer> UNIDADES = Arrays.asList(IDS_UNIDADES);
 }
